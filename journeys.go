d65c909358efdd8dd5437e2b2011dd168f84f68c
taimats <beo8939@gmail.com>
2024-11-29 17:30:14 +0900

    JourneysにPublicメソッドを追加

--- journeys.go
+++ journeys.go
@@ -1,5 +1,7 @@
 package meander
 
+import "strings"
+
 type j struct {
 	Name        string
 	PlacesTypes []string
@@ -9,4 +11,11 @@ var Journeys = []any{
 	&j{Name: "ロマンティック", PlacesTypes: []string{"park", "bar", "movie_theater", "restaurant", "florist", "taxi_stand"}},
 	&j{Name: "ショッピング", PlacesTypes: []string{"department_store", "cafe", "clothng_store", "jewely_store", "shoe_store"}},
 	&j{Name: "ロマンティック", PlacesTypes: []string{"bar", "casino", "food", "bar", "night_club"}},
+}
+
+func (j *j) Public() any {
+	return map[string]any{
+		"name":    j.Name,
+		"journey": strings.Join(j.PlacesTypes, "|"),
+	}
 }
